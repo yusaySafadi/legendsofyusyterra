07dd037b1ce008a7c0209de2832847b9e6101290
John Doe <yusay.safadi@gmail.com>
2022-09-02 21:56:43 +0200

    changedfile strucutre a bit

--- frontend/src/components/GameMatch.jsx
+++ frontend/src/components/GameMatch.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledGameMatchConatiner = styled.div`
-color: #e2e1e1;
-width: 50%;
+  height: 10%;
   display: flex;
-  border: 2px solid black;
   align-items: center;
+  justify-content: space-between;
+  color: #e2e1e1;
+  width: 50%;
+  border: 2px solid black;
   border-radius: 15px;
+  background: ${(props) =>
+    props.outcome === "loss" ? "rgb(255,0,65)" : "rgb(0,224,255)"};
+  background: ${(props) =>
+    props.outcome === "loss"
+      ? "linear-gradient(90deg, rgba(255,0,65,1) 0%, rgba(0,224,255,1) 100%)"
+      : "linear-gradient(90deg, rgba(0,224,255,1) 0%, rgba(255,0,68,1) 100%)"};
+
   > * {
     border: 1px solid black;
   }
-background: ${props => props.outcome === "loss" ? "rgb(255,0,65)": "rgb(0,224,255)"};
-background: ${props => props.outcome === "loss" ? 'linear-gradient(90deg, rgba(255,0,65,1) 0%, rgba(0,224,255,1) 100%)' 
-: 'linear-gradient(90deg, rgba(0,224,255,1) 0%, rgba(255,0,68,1) 100%)'};
 `;
+
 const StyledFractionImage = styled.img`
-width: 50px;
-height: 50px;
-`
+  width: 36px;
+  height: 36px;
+`;
 
 export default function GameMatch({ game }) {
   const fractionRegex = "_([a-zA-Z0-9.-]*)_";
@@ -26,14 +33,16 @@ export default function GameMatch({ game }) {
     fraction.match(fractionRegex)[1].toLowerCase()
   );
   let enemyFractions = game.info.players[0].factions.map((fraction) =>
-    fraction.match(fractionRegex)[1].toLowerCase());
+    fraction.match(fractionRegex)[1].toLowerCase()
+  );
   return (
     <StyledGameMatchConatiner outcome={game.info.players[1].game_outcome}>
-      <p>{game.info.game_mode}</p>
+      <p>{game.info.game_type}</p>
+      <div>
+        <p>Game Result</p>
+        <h3>{game.info.players[1].game_outcome}</h3>
+      </div>
 
-      <p>Game Result</p>
-
-      <h3>{game.info.players[1].game_outcome}</h3>
       <div>
         <h3>Regions</h3>
         <div>
@@ -55,7 +64,6 @@ export default function GameMatch({ game }) {
           ))}
         </div>
       </div>
-
     </StyledGameMatchConatiner>
   );
 }
