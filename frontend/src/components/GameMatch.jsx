330d8a7b2e6998fbac6c742dad3bb4b878916d49
John Doe <yusay.safadi@gmail.com>
2022-09-02 21:13:22 +0200

    added styles and gradient to GameMatch component

--- frontend/src/components/GameMatch.jsx
+++ frontend/src/components/GameMatch.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledGameMatchConatiner = styled.div`
+color: #e2e1e1;
+width: 50%;
   display: flex;
   border: 2px solid black;
   align-items: center;
@@ -9,6 +11,9 @@ const StyledGameMatchConatiner = styled.div`
   > * {
     border: 1px solid black;
   }
+background: ${props => props.outcome === "loss" ? "rgb(255,0,65)": "rgb(0,224,255)"};
+background: ${props => props.outcome === "loss" ? 'linear-gradient(90deg, rgba(255,0,65,1) 0%, rgba(0,224,255,1) 100%)' 
+: 'linear-gradient(90deg, rgba(0,224,255,1) 0%, rgba(255,0,68,1) 100%)'};
 `;
 const StyledFractionImage = styled.img`
 width: 50px;
@@ -20,9 +25,10 @@ export default function GameMatch({ game }) {
   let playerFractions = game.info.players[1].factions.map((fraction) =>
     fraction.match(fractionRegex)[1].toLowerCase()
   );
-  console.log(game.info.players[1].factions[0].match(fractionRegex)[1]);
+  let enemyFractions = game.info.players[0].factions.map((fraction) =>
+    fraction.match(fractionRegex)[1].toLowerCase());
   return (
-    <StyledGameMatchConatiner>
+    <StyledGameMatchConatiner outcome={game.info.players[1].game_outcome}>
       <p>{game.info.game_mode}</p>
 
       <p>Game Result</p>
@@ -39,6 +45,16 @@ export default function GameMatch({ game }) {
         </div>
       </div>
       <h2>VS</h2>
+      <div>
+        <h3>Regions</h3>
+        <div>
+          {enemyFractions.map((fr) => (
+            <StyledFractionImage
+              src={`https://dd.b.pvp.net/3_14_0/core/en_us/img/regions/icon-${fr}.png`}
+            ></StyledFractionImage>
+          ))}
+        </div>
+      </div>
 
     </StyledGameMatchConatiner>
   );
