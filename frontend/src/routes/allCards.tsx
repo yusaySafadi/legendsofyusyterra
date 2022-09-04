a44ba3a822c5d68166789e3db95e07fce6774a95
John Doe <yusay.safadi@gmail.com>
2022-09-04 23:42:21 +0200

    added basic styling to allCards

--- frontend/src/routes/allCards.tsx
+++ frontend/src/routes/allCards.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import {Outlet}from "react-router-dom"
-import { GameData } from '../interfaces';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { GameData } from "../interfaces";
 
-export default function AllCards(){
-    const [data, setData] = useState<GameData[]>([]);
-  useEffect(()=>{
-    fetch("http://localhost:5000/getData").then((res) => res.json()).then((d)=> setData(d));
-  },[])
-    return(
-        <>
-        {JSON.stringify(data) !== '{}'? console.log(typeof([])) : <p>Loading....</p>}
-      {JSON.stringify(data) !== '{}'? data.map((f)=>(<img src={f.assets[0].gameAbsolutePath} alt=""></img>)): <p>Loading....</p>}
-      {/*JSON.stringify(data) !== '{}'? data.vocabTerms.map((f) => (
-          <p> {f.name} : {f.description}</p>
-      )): <p>Loading...</p>*/}
-      <Outlet/>
-        </>
-    )
+export default function AllCards() {
+  const [data, setData] = useState<GameData[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/getData")
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  }, []);
+  return (
+    <>
+      {JSON.stringify(data) !== "{}" ? (
+        data.map((f) => <StyledCardImage src={f.assets[0].gameAbsolutePath} alt="" />)
+      ) : (
+        <p>Loading....</p>
+      )}
+      <Outlet />
+    </>
+  );
 }
+
+const StyledCardGallery  = styled.div`
+display: flex;
+width: 40%;
+`
+const StyledCardImage = styled.img`
+height: 250px;
+width: auto;
+`
